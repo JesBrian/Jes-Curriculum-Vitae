9624d669209131878fa6b7bbf4d54d8e03d63a77
JesBrian <JesBrian666@gmail.com>
2018-04-24 15:10:39 +0800

    PHP基础 - [ 部分mysqli操作 & 日期时间操作 ]

--- PHP - Base/Date.php
+++ PHP - Base/Date.php
@@ -6,3 +6,49 @@
  * Date: 2018-04-20
  * Time: 17:16
  */
+
+/**
+ * Notes: 设置 PHP 默认时区
+ * User: JesBrian
+ * Date: 2018-04-24
+ * Time: 14:41
+ */
+function setTimeDefaultZone()
+{
+    // php.ini 全局设置
+//    [Date]
+//    date.timezone = PRC
+
+    // 运行脚本设置
+    date_default_timezone_set('PRC');
+    echo date_default_timezone_get() . PHP_EOL;
+}
+setTimeDefaultZone();
+
+
+/**
+ * Notes: 时间日期基本操作
+ * User: JesBrian
+ * Date: 2018-04-24
+ * Time: 15:09
+ */
+function dateBaseOper()
+{
+    // time() 返回当前时间戳
+    // date('Y:m:d H:i:s', $timeStamp) 格式化时间戳，如果不传入时间戳，默认为当前时间戳
+    $oldNowTime = time();
+    echo $oldNowTime . PHP_EOL;
+    echo date('Y:m:d H:i:s', $oldNowTime) . PHP_EOL;
+    sleep(2);
+    echo date('Y:m:d H:i:s') . PHP_EOL;
+
+    // strtotime 将日期字符串转为时间戳 - 但不是很智能 [ 具体百度 ]
+    $timeStr = strtotime('2018-08-02 02:06:01');
+    echo date('Y:m:d H:i:s', $timeStr) . PHP_EOL;
+
+    // getdate($timestamp) 获取日期时间信息，不传入时间戳默认为当前时间
+    // [ seconds - 秒 / minutes - 分 / hours - 时 / mday - 月份第几天 / wday - 星期第几天 / mon - 月份(数值) / year - 4位数年份 / yday - 年第几天 / weekday - 星期(英语) / month - 月份(英语) / 0 - 时间戳 ]
+    var_dump(getdate());
+
+}
+dateBaseOper();
--- PHP - Base/MySQLi.php
+++ PHP - Base/MySQLi.php
@@ -1,8 +1,95 @@
 <?php
 /**
- * PHP 基础 - MySQLi 数据库基本操作
+ * PHP 基础 - MySQLi 数据库基本操作 - 数据库基础文件在该目录下的 test.sql
  * Created by PhpStorm.
  * User: JesBrian
  * Date: 2018-04-20
  * Time: 17:15
  */
+
+/**
+ * Notes: 数据库连接操作
+ * User: JesBrian
+ * Date: 2018-04-23
+ * Time: 16:10
+ * @param $host
+ * @param $username
+ * @param $passwd
+ * @param $dbNamem
+ * @param $port
+ * @return mysqli
+ */
+function dbConnect($host, $username, $passwd, $dbName, $port)
+{
+    $dbConnect = new mysqli($host, $username, $passwd, $dbName, $port);
+    if ($dbConnect->connect_error) {
+        die("MySQL连接失败！" . $dbConnect->connect_error);
+    } else {
+        echo 'MySQL连接成功！' . PHP_EOL;
+    }
+    return $dbConnect;
+}
+
+
+/**
+ * Notes: 两种 插入数据 操作 - query($sql) / prepare($sql) -> bind_param($value) -> execute()
+ * User: JesBrian
+ * Date: 2018-04-23
+ * Time: 16:18
+ * @param $dbConnect
+ */
+function insertOper($dbConnect)
+{
+    // 原生 SQL
+    $sql = "INSERT INTO phpbase_test.test_tab_a(name, age) VALUES('JesBrian',21);";
+    if ($dbConnect->query($sql) === true) {
+        echo '插入数据成功！' . PHP_EOL;
+    } else {
+        echo '插入数据失败！' . $dbConnect->error;
+    }
+
+    // 参数绑定
+    $prepareSql = "INSERT INTO test_tab_a(name, age) VALUES(?,?);";
+    $stmt = $dbConnect->prepare($prepareSql);
+    $name = 'JesBrian';
+    $age = 22;
+    $stmt->bind_param('ss',$name, $age);
+    if ($stmt->execute() === true) {
+        echo '插入数据成功！' . PHP_EOL;
+    } else {
+        echo '插入数据失败！' . $dbConnect->error;
+    }
+    $name = 'JesBrian2';
+    $age = 223;
+    if ($stmt->execute() === true) {
+        echo '插入数据成功！' . PHP_EOL;
+    } else {
+        echo '插入数据失败！' . $dbConnect->error;
+    }
+}
+
+
+/**
+ * Notes: 查询操作
+ * User: JesBrian
+ * Date: 2018-04-23
+ * Time: 16:17
+ * @param $dbConnect
+ */
+function selectOper($dbConnect)
+{
+
+}
+
+
+
+$host = '127.0.0.1';
+$username = 'root';
+$passwd = 'JesBrian';
+$dbName = 'phpbase_test';
+$port = '3306';
+
+$dbConnect = dbConnect($host, $username, $passwd, $dbName, $port);
+insertOper($dbConnect);
+
+$dbConnect->close(); // 关闭数据库连接 ！！！！
