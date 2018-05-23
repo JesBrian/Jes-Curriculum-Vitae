2418a35a44a3625c07cd0eaef44af965703c9e77
JesBrian <jesbrian666@gmail.com>
2018-05-23 15:43:07 +0800

    设计模式 - [ update 工厂模式 ]

--- DesignPattern/FactoryPattern/Index.php
+++ DesignPattern/FactoryPattern/Index.php
@@ -13,6 +13,84 @@
  * Time: 17:17
  */
 
+interface userProperties {
+    function getUsername();
+    function getGender();
+    function getJob();
+}
 
+interface createUser {
+    function create($properties);
+}
 
+class User implements userProperties{
+    private $username;
+    private $gender;
+    private $job;
+    public function __construct($username, $gender, $job) {
+        $this->username = $username;
+        $this->gender = $gender;
+        $this->job = $job;
+    }
+
+    public function getUsername() {
+        return $this->username;
+    }
+
+    public function getGender() {
+        return $this->gender;
+    }
+
+    public function getJob() {
+        return $this->job;
+    }
+}
+
+class userFactory {
+    private $user;
+    public function __construct($properties = []) {
+        $this->user =  new User($properties['username'], $properties['gender'], $properties['job']);
+    }
+
+    public function getUser() {
+        return $this->user;
+    }
+}
+
+class FactoryMan implements createUser {
+    function create($properties) {
+        return new userFactory($properties);
+    }
+}
+
+class FactoryWoman implements createUser {
+    function create($properties) {
+        return new userFactory($properties);
+    }
+}
+
+class clientUser {
+    static public function getManClient($properties) {
+        $fac = new FactoryMan;
+        $man = $fac->create($properties);
+        echo $man->getUser()->getUsername() . ' - ';
+        echo $man->getUser()->getGender() . ' - ';
+        echo $man->getUser()->getJob();
+    }
+    static public function getWomanClient($properties) {
+        $fac = new FactoryMan;
+        $man = $fac->create($properties);
+        echo $man->getUser()->getUsername() . ' - ';
+        echo $man->getUser()->getGender() . ' - ';
+        echo $man->getUser()->getJob();
+    }
+}
+
+$employers = [
+    ['username' => 'Jack', 'gender' => 'male', 'job' => 'coder'],
+    ['username' => 'Marry', 'gender' => 'female', 'job' => 'designer'],
+];
+clientUser::getManClient($employers[0]);
+echo PHP_EOL;
+clientUser::getWomanClient($employers[1]);
 
