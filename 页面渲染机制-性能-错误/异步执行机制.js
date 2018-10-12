0db89c12d04aecc489dea3feb4eca513d1f8f6ea
JesBrian <jesbrian666@gmail.com>
2018-10-12 23:07:42 +0800

    JavaScript - [ 异步操作的执行机制 ]

---
+++ 页面渲染机制-性能-错误/异步执行机制.js
@@ -0,0 +1,32 @@
+
+// 4444 - 0123
+// for (var i = 0; i < 4; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+// for (let i = 0; i < 4; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   }, 1000);
+// }
+
+
+// A B C D
+// console.log('A');
+// setTimeout(() => {
+//   console.log('D');
+// });
+// console.log('B');
+// console.log('C');
+
+
+// 只输出A
+// console.log('A');
+// setTimeout(() => {
+//   console.log('B');
+// });
+// while (true) {}
+
+
+
