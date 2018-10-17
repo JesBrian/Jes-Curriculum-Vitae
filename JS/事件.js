7903c1bc6c6b693a72fb0cd8ecf570282f5f63a1
JesBrian <jesbrian666@gmail.com>
2018-10-17 12:34:28 +0800

    info - [ update ]

--- JS/事件.js
+++ JS/事件.js
@@ -33,34 +33,34 @@ event.preventDefault(); // 阻止默认事件 - 表单提交/a标签跳转
 event.stopPropagation(); // 阻止事件冒泡
 event.currentTarget // 事件代理 - 获取具体点击的元素
 event.target // 获取当前事件标签 - 输入框/单复选值 event.target.value
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="referrer" content="origin">
-<title>【前端】</title>
-</head>
-<body>
-<ul id="list">
-    <li id="li-1">Li 2</li>
-    <li id="li-2">Li 3</li>
-    <li id="li-3">Li 4</li>
-    <li id="li-4">Li 5</li>
-    <li id="li-5">Li 6</li>
-    <li id="li-6">Li 7</li>
-</ul>
-
-
-	<script>
-		document.getElementById('list').addEventListener('click', function(e) {
-			console.log(e.currentTarget); // ul
-			console.log(e.target); // li
-		})
-	</script>
-
-</body>
-</html>
+// <!DOCTYPE html>
+// <html>
+// <head>
+// <meta charset="utf-8">
+// <meta name="viewport" content="width=device-width, initial-scale=1">
+// <meta name="referrer" content="origin">
+// <title>【前端】</title>
+// </head>
+// <body>
+// <ul id="list">
+//     <li id="li-1">Li 2</li>
+//     <li id="li-2">Li 3</li>
+//     <li id="li-3">Li 4</li>
+//     <li id="li-4">Li 5</li>
+//     <li id="li-5">Li 6</li>
+//     <li id="li-6">Li 7</li>
+// </ul>
+//
+//
+// 	<script>
+// 		document.getElementById('list').addEventListener('click', function(e) {
+// 			console.log(e.currentTarget); // ul
+// 			console.log(e.target); // li
+// 		})
+// 	</script>
+//
+// </body>
+// </html>
 
 
 /**
